e5bbef5934095fd5a9453a9a1131d40da047faa1
lizzy <lizzyphy@163.com>
2017-04-24 15:23:34 +0800

    舞台搭建

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/Main';
+
+//alert('11111111111111');
+// Render the main component into the dom
+ReactDOM.render(<App />, document.getElementById('app'));
+//alert('aaaaaaaaaa');
